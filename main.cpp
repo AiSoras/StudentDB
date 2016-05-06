8b6dec69943894db2ca4267dd1902019cf15b74a
Olesia <5otaisora@gmail.com>
2016-05-06 14:55:53 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,13 +1,13 @@
-#include "Lib.h"
+#include "lib.h"
 
 char point[] = "->";
 int amount = 0, N, fmenu, smenu, stop, gmenu, stmenu, dbmenu, filemenu;
-COORD m = { 0, 0 }; //Положение меню
-COORD c = { 0, 1 }; //Положение стрелки
+COORD m = { 0, 0 }; //ГЏГ®Г«Г®Г¦ГҐГ­ГЁГҐ Г¬ГҐГ­Гѕ
+COORD c = { 0, 1 }; //ГЏГ®Г«Г®Г¦ГҐГ­ГЁГҐ Г±ГІГ°ГҐГ«ГЄГЁ
 HANDLE h = GetStdHandle(STD_OUTPUT_HANDLE);
-Group* start = NULL; //Начало всего потока
-Group* finish = NULL; //Конец потока
-Group* fordel = NULL; //Группа, где необходимо удалить студента
+Group* start = NULL; //ГЌГ Г·Г Г«Г® ГўГ±ГҐГЈГ® ГЇГ®ГІГ®ГЄГ 
+Group* finish = NULL; //ГЉГ®Г­ГҐГ¶ ГЇГ®ГІГ®ГЄГ 
+Group* fordel = NULL; //ГѓГ°ГіГЇГЇГ , ГЈГ¤ГҐ Г­ГҐГ®ГЎГµГ®Г¤ГЁГ¬Г® ГіГ¤Г Г«ГЁГІГј Г±ГІГіГ¤ГҐГ­ГІГ 
 void main()
 {
 	FirstMenu();
